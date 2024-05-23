8a64b211bcab73a0ff211add5ff1924a17555447
Coco Myrén Hallström <coco.myrenhallstrom@learnet.se>
2024-05-23 12:37:21 +0200

    Seeds can kinda "fuse"

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,12 +7,7 @@ using UnityEngine.EventSystems;
 public class Item : MonoBehaviour
 {
 
-    bool canMove = true;
-    Vector3 moveDirection = Vector3.zero;
-    CharacterController characterController;
-
-    [SerializeField]
-    float gravity = 20.0f;
+    static bool colidedOnce = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,29 +18,25 @@ public class Item : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // We are grounded, so recalculate move direction based on axes
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Vector3 right = transform.TransformDirection(Vector3.right);
 
 
-        float movementDirectionY = moveDirection.y;
+    }
 
-
-        moveDirection.y = movementDirectionY;
-
-
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
-        if (!characterController.isGrounded)
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("canPickUp"))
         {
-            moveDirection.y -= gravity * Time.deltaTime;
+            if (Item.colidedOnce == false)
+            {
+                Item.colidedOnce = true;
+                Debug.Log("Item Collided");
+            }
+            else
+            {
+                Item.colidedOnce = false;
+                Destroy(gameObject);
+            }
         }
-
-        // Move the controller
-        characterController.Move(moveDirection * Time.deltaTime);
-
-
 
     }
 }
